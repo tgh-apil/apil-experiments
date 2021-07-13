df5dd931741dd648739e2a7dda9ee2be72dcf951
Joshua Hiansen <jhiansen@JDMI.ca>
2021-07-13 15:28:26 -0400

    testings for name of glb when hovering

--- src/components/ModelViewer.component.svelte
+++ src/components/ModelViewer.component.svelte
@@ -442,6 +442,7 @@
             
             INTERSECTED = null;
             structureName = 'No structure selected';
+            console.log(INTERSECTED.name);
         }
 
 		renderer.render(scene, camera);
